d7c73e99f8e2bcb891dbbc9ef58f64527237bdbf
Ashley Cawley <acawley@7669.co.uk>
2018-10-26 19:54:33 +0000

    Added syntax highlighting to switch it on

--- install-fix-and-self-remove.sh
+++ install-fix-and-self-remove.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo ":set mouse=" >> ~/.vimrc
+echo ":syntax on" >> ~/.vimrc
 
 # This script removes itself
 rm -f ./install-fix-and-self-remove.sh
--- install-fix.sh
+++ install-fix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo ":set mouse=" >> ~/.vimrc
+echo ":syntax on" >> ~/.vimrc
 exit 0
